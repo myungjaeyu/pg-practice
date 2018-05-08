08c170cb26196eeb625e5bd62392f8f65edd6421
u4bi <admin@naver.id>
2018-05-08 16:59:45 +0900

    CafeReview - Added insert query.

--- cafe-review.sql
+++ cafe-review.sql
@@ -18,3 +18,25 @@ CREATE TABLE IF NOT EXISTS CafeReview( id SERIAL PRIMARY KEY,
 -- \d Cafe
 -- \d Reviewer
 -- \d CafeReview
+
+
+INSERT 
+  INTO Cafe
+VALUES (default, 'A 카페', '거리'),
+       (default, 'B 카페', '거리'),
+       (default, 'C 카페', '거리');
+
+INSERT 
+  INTO Reviewer
+VALUES (default, '김 MH', 'mh@gmail.com'),
+       (default, '강 ND', 'nd@gmail.com'),
+       (default, '박 KA', 'ka@gmail.com'),
+       (default, '이 BW', 'bw@gmail.com');
+
+INSERT 
+  INTO CafeReview
+VALUES (default, 1, 1, 5, '리뷰 제목', '리뷰 내용'),
+       (default, 1, 2, 5, '리뷰 제목', '리뷰 내용'),
+       (default, 2, 3, 5, '리뷰 제목', '리뷰 내용'),
+       (default, 2, 4, 3, '리뷰 제목', '리뷰 내용'),
+       (default, 3, 4, 3, '리뷰 제목', '리뷰 내용');
